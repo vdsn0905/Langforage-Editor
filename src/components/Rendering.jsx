1ecf2bd2b8a2b642e19e4c8891b775994be326b9
vdsn0905 <vdsn0905@gmail.com>
2025-09-23 20:24:52 +0530

    Update Rendering.jsx

--- src/components/Rendering.jsx
+++ src/components/Rendering.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Rendering() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 place-content-center">
-      <img src="/s.gif" alt="" className="place-self-center" />
+      <img src="/OVTb.gif" alt="" className="place-self-center" />
     </div>
   );
 }
+
