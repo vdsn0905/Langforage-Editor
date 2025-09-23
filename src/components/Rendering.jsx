67bfc8ca2920b4ff7ca95a98e2dab7b846848db2
vdsn0905 <vdsn0905@gmail.com>
2025-09-23 20:39:49 +0530

    Add files via upload

--- src/components/Rendering.jsx
+++ src/components/Rendering.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function Rendering() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 place-content-center">
-      <img src="/OVTb.gif" alt="" className="place-self-center" />
+      <img src="/loading.gif" alt="" className="place-self-center" />
     </div>
   );
 }
-
